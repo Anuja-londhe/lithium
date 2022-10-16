0090cb64a6418a3df9ddee2adcf97adc51355331
anuja-londhe <anujadlondhe@gmail.com>
2022-10-16 20:49:02 +0530

    assignment modules

---
+++ src/logger/logger.js
@@ -0,0 +1,7 @@
+function welcome() {
+    console.log(
+     "Welcome to my application.I am anuja londhe I am a part of Functionup Lithium Cohort"
+    )
+}
+
+module.exports.welcome = welcome
--- src/routes/route.js
+++ src/routes/route.js
@@ -4,11 +4,21 @@ const router = express.Router();///test-you
 const xyz = require('../logger')
 //importing external package
 const underscore = require('underscore')
+//import module
+const wel = require("../logger/logger")
+const help = require("../util/helper");
+const format = require("../validator/formatter")
+const lodash = require("../lodash/lodash")
 
 router.get('/test-me', function (req, res) {
     //Calling the components of a different custom module
     console.log("Calling my function ",xyz.myFunction())
     console.log("The value of the constant is ",xyz.myUrl)
+    //calling the function
+    wel.welcome()
+    help.getbatchInfo();
+    format.format();
+    lodash.lodash()
     //Trying to use an external package called underscore
     let myArray = ['Akash', 'Pritesh', 'Sabiha']
     let result = underscore.first(myArray)
